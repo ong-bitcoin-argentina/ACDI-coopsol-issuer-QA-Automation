2d9dfcc47c5226fdcc4027951bad3e012458dcef
ENahuelPaczkowski <nahuelpaco@gmail.com>
2022-12-21 18:33:27 -0300

    visualización de solicitudes
    se completó el caso de visualización de solicitudes

--- src/main/java/lippia/web/constants/ValidacionDeIdentidadConstants.java
+++ src/main/java/lippia/web/constants/ValidacionDeIdentidadConstants.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 
 public class ValidacionDeIdentidadConstants {
     public static final String GENERIC_BUTTON = "xpath://*[contains(text(),'%s')]";
-    public static final String SOLICITUDES_APROBADAS = "id:rc-tabs-1-panel-2";
+
+    public static final String SOLICITUDES_PANEL = "id:rc-tabs-1-panel-%s";
 
 }
